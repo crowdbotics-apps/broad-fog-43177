a7ca3f3d2a9cf2a626007b444e22fac59ce03301
Crowdbotics <team@crowdbotics.com>
2023-08-31 23:54:53 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "broad-fog-43177",
+  url: "https://broad-fog-43177.botics.co",
+  api: "https://broad-fog-43177.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
